c92c95d5714af8ddc31cf0c18106993eeacdeb2f
Jatin2102786 <jatin@o7tec.com>
2026-01-24 15:12:29 +0530

    Drawer Layout

---
+++ app/src/main/java/com/o7solutions/android_6_month/Drawer/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.o7solutions.android_6_month.Drawer.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
